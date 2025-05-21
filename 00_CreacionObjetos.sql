4e1c02ea81a3fdf4d059cce6e6a253c7445452f8
GrozzoP <95724294+GrozzoP@users.noreply.github.com>
2025-05-21 08:57:44 -0300

    Creacion de los SP para la mitad de las tablas y modificacion en el nombramiento de algunos campos

--- 00_CreacionObjetos.sql
+++ 00_CreacionObjetos.sql
@@ -15,7 +15,7 @@ else
 	end
 go
 
-Use COM5600G03
+use COM5600G03
 go
 
 if exists(
@@ -58,14 +58,14 @@ else
 go
 
 IF OBJECT_ID('socios.rol', 'U') IS NULL
-Begin
+begin
 	Create table socios.rol(
 		id_rol int,
 		nombre varchar(40)	UNIQUE,
 		descripcion varchar(200),
 		Constraint Socios_rol_PK_id_rol Primary key(id_rol)
 	)
-End
+end
 else
 begin
 	print 'La tabla socios.rol ya existe'
@@ -73,14 +73,14 @@ end
 go
 
 IF OBJECT_ID('facturacion.medio_de_pago', 'U') IS NULL
-Begin
+begin
 	Create table facturacion.medio_de_pago(
 		id_medio_de_pago int identity(1,1),
 		nombre_medio_pago varchar(40),
 		permite_debito_automatico bit --posible check para entender mejor
 		Constraint Facturacion_medio_de_pago_PK_id_medio_de_pago Primary key(id_medio_de_pago)
 	)
-End
+end
 else
 begin
 	print 'La tabla facturacion.medio_de_pago ya existe'
@@ -88,16 +88,16 @@ end
 go
 
 IF OBJECT_ID('socios.usuario', 'U') IS NULL
-Begin
+begin
 	Create table socios.usuario(
-		id_user int identity(1,1),
+		id_usuario int identity(1,1),
 		id_rol int,
 		contraseña varchar(40),
 		fecha_vigencia_contraseña date,
-		Constraint socios_usuario_PK_id_user Primary key(id_user),
+		Constraint socios_usuario_PK_id_user Primary key(id_usuario),
 		Constraint socios_usuario_FK_id_rol Foreign Key(id_rol) References socios.rol(id_rol)
 	)
-End
+end
 else
 begin
 	print 'La tabla socios.usuario ya existe'
@@ -123,7 +123,7 @@ IF OBJECT_ID('socios.categoria', 'U') IS NULL
 Begin
 	Create table socios.categoria(
 		id_categoria int identity(1,1),
-		nombreCategoria varchar(16) UNIQUE,
+		nombre_categoria varchar(16) UNIQUE,
 		edad_minima int,
 		edad_maxima int,
 		costo_membresía decimal(9,3)
@@ -155,7 +155,7 @@ Begin
 		Constraint Socios_socio_PK_id_socio Primary key(id_socio),
 		Constraint Socios_socio_FK_id_obra_social Foreign Key(id_obra_social) References socios.obra_social(id_obra_social),
 		Constraint Socios_socio_FK_categoria Foreign Key(id_categoria) References socios.categoria(id_categoria),
-		Constraint Socios_socio_FK_usuario Foreign Key(id_usuario) References socios.usuario(id_user),
+		Constraint Socios_socio_FK_usuario Foreign Key(id_usuario) References socios.usuario(id_usuario),
 		Constraint Socios_socio_FK_medio_de_pago Foreign Key(id_medio_de_pago) References facturacion.medio_de_pago(id_medio_de_pago)
 	)
 End
@@ -168,8 +168,8 @@ go
 IF OBJECT_ID('socios.responsable_menor', 'U') IS NULL
 Begin
 	Create table socios.responsable_menor(
-		id_socio_menor int identity(1,1),--podemos tener 2 socios con el mismo id, pero uno es menor y el otro es mayor (?)
-		id_socio_responsable int,
+		id_socio_responsable int identity(1,1),
+		id_socio_menor int,
 		nombre varchar(40),
 		apellido varchar(40),
 		DNI int,
@@ -177,12 +177,10 @@ Begin
 		fecha_nacimiento date,
 		telefono int,
 		parentesco varchar(30),
-		Constraint Socios_responsable_menor_PK_id_socio_menor
-				Primary key(id_socio_menor),
+		Constraint Socios_responsable_menor_PK_id_socio_responsable
+				Primary key(id_socio_responsable),
 		Constraint Socios_responsable_menor_FK_id_socio_menor
-				Foreign key(id_socio_menor) References socios.socio(id_socio),
-		Constraint Socios_responsable_menor_FK_id_socio_responsable
-				Foreign Key(id_socio_responsable) References socios.socio(id_socio)
+				Foreign key(id_socio_menor) References socios.socio(id_socio)
 	)
 End
 else
@@ -195,7 +193,7 @@ IF OBJECT_ID('actividades.actividad', 'U') IS NULL
 Begin
 	Create table actividades.actividad(
 		id_actividad int identity(1,1),
-		nombreActividad varchar(36) UNIQUE,
+		nombre_actividad varchar(36) UNIQUE,
 		costo_mensual decimal(9,3)
 		Constraint Actividades_actividad_PK_id_actividad Primary key(id_actividad)
 	)
@@ -210,7 +208,7 @@ IF OBJECT_ID('actividades.actividad_extra', 'U') IS NULL
 Begin
 	Create table actividades.actividad_extra(
 		id_actividad int identity(1,1),
-		nombreActividad varchar(36) UNIQUE,
+		nombre_actividad varchar(36) UNIQUE,
 		costo decimal(9,3)
 		Constraint Actividades_actividad_extra_PK_id_actividad Primary key(id_actividad)
 	)
@@ -221,8 +219,8 @@ begin
 end
 go
 
-IF OBJECT_ID('actividades.horario_actividades', 'U') IS NULL
-Begin
+if OBJECT_ID('actividades.horario_actividades', 'U') IS NULL
+begin
 	Create table actividades.horario_actividades(
 		id_horario int identity(1,1),
 		dia_semana varchar(18),
@@ -236,15 +234,15 @@ Begin
 		Constraint Actividades_horario_actividades_FK_id_categoria
 				Foreign Key(id_categoria) References socios.categoria(id_categoria)
 	)
-End
+end
 else
 begin
 	print 'La tabla actividades.horario_actividades ya existe'
 end
 go
 
-IF OBJECT_ID('actividades.inscripcion_actividades', 'U') IS NULL
-Begin
+if OBJECT_ID('actividades.inscripcion_actividades', 'U') IS NULL
+begin
 	Create table actividades.inscripcion_actividades(
 		id_inscripcion int identity(1,1),
 		id_horario int,
@@ -258,16 +256,16 @@ Begin
 		Constraint Actividades_inscripcion_actividades_FK_id_actividad
 				Foreign Key(id_actividad) References actividades.actividad(id_actividad)
 	)
-End
+end
 else
 begin
 	print 'La tabla actividades.inscripcion_actividades ya existe'
 end
 go
 
-IF OBJECT_ID('actividades.inscripcion_act_extra', 'U') IS NULL
-Begin
-	Create table actividades.inscripcion_act_extra(
+if OBJECT_ID('actividades.inscripcion_act_extra', 'U') IS NULL
+begin
+	create table actividades.inscripcion_act_extra(
 		id_inscripcion_extra int identity(1,1),
 		id_socio int,
 		id_actividad_extra int,
@@ -281,15 +279,15 @@ Begin
 		Constraint Actividades_inscripcion_actividades_FK_id_actividad_extra
 				Foreign Key(id_actividad_extra) References actividades.actividad_extra(id_actividad)
 	)
-End
+end
 else
 begin
 	print 'La tabla actividades.inscripcion_act_extra ya existe'
 end
 go
 
-IF OBJECT_ID('facturacion.factura', 'U') IS NULL
-Begin
+if OBJECT_ID('facturacion.factura', 'U') IS NULL
+begin
 	Create table facturacion.factura(
 		id_factura int identity(1,1),
 		fecha_emision date,
@@ -306,19 +304,19 @@ Begin
 		Constraint Facturacion_factura_FK_id_socio
 				Foreign Key(id_socio) References socios.socio(id_socio)
 	)
-End
+end
 else
 begin
 	print 'La tabla facturacion.factura ya existe'
 end
 go
 
-IF OBJECT_ID('facturacion.pago', 'U') IS NULL
-Begin
+if OBJECT_ID('facturacion.pago', 'U') IS NULL
+begin
 	Create table facturacion.pago(
 		id_pago int identity(1,1),
 		fecha_pago date,
-		montoTotal decimal(9,3),
+		monto_total decimal(9,3),
 		id_factura int,
 		tipo_movimiento varchar(20),
 		--id_medio_pago int,
@@ -326,7 +324,7 @@ Begin
 		Constraint Facturacion_pago_FK_id_factura
 				Foreign Key(id_factura) References facturacion.factura(id_factura)
 	)
-End
+end
 else
 begin
 	print 'La tabla facturacion.pago ya existe'
--- 01_CreacionSPs.sql
+++ 01_CreacionSPs.sql
@@ -1,3 +1,426 @@
 Use COM5600G03
 go
---generacion de los sp
+-- ROL
+-- Procedimiento para insertar un rol
+create or alter procedure socios.insertarRol(@nombre_rol varchar(20), @descripcion_rol varchar(50))
+as
+begin
+	if exists(select 1 from socios.rol 
+			where nombre = @nombre_rol)
+	begin
+		print 'El rol que se quiere insertar ya existe en la tabla.'
+	end
+	else
+	begin
+		insert into socios.rol(nombre, descripcion) values (@nombre_rol, @descripcion_rol)
+	end
+end
+go
+
+-- Procedimiento para modificar un rol
+create or alter procedure socios.modificarRol(@nombre_rol varchar(20), @nueva_descripcion_rol varchar(50))
+as
+begin
+	if not exists(select 1 from socios.rol 
+				where nombre = @nombre_rol)
+	begin
+		print 'El rol que se quiere modificar, no existe segun su nombre.'
+	end
+	else
+	begin
+		update socios.rol
+		set descripcion = @nueva_descripcion_rol
+		where nombre = @nombre_rol
+	end
+end
+go
+
+-- Procedimiento para eliminar un rol de la tabla
+create or alter procedure socios.eliminarRol(@nombre_rol varchar(20))
+as
+begin
+	if not exists(select 1 from socios.rol 
+				where nombre = @nombre_rol)
+	begin
+		print 'El rol que se quiere eliminar, no existe segun su nombre.'
+	end
+	else
+	begin
+		delete from socios.rol
+		where nombre = @nombre_rol
+	end
+end
+go
+
+-- MEDIO DE PAGO
+-- Procedimiento para insertar un medio de pago
+create or alter procedure facturacion.crearMedioPago(@nombre_medio_de_pago varchar(40), @permite_debito_automatico BIT)
+as
+begin
+	if not exists(select 1 from facturacion.medio_de_pago
+				where nombre_medio_pago = @nombre_medio_de_pago)
+	begin
+		print 'El medio de pago que se quiere agregar, ya existe.'
+	end
+	else
+	begin
+		insert into facturacion.medio_de_pago(nombre_medio_pago, permite_debito_automatico)
+		values (@nombre_medio_de_pago, @permite_debito_automatico)
+	end
+end
+go
+
+-- Procedimiento para modificar un medio de pago
+create or alter procedure facturacion.modificarMedioPago(@nombre_medio_de_pago varchar(40))
+as
+begin
+	if not exists(select 1 from facturacion.medio_de_pago
+				where nombre_medio_pago = @nombre_medio_de_pago)
+	begin
+		print 'El medio de pago que se quiere modificar, no existe.'
+	end
+	else
+	begin
+		/* Como tenemos un campo de 'bit' y solo tiene dos valores posibles, si lo quiero modificar, simplemente
+		   niego el bit actual vinculado con el habilitar */
+		update facturacion.medio_de_pago
+		set permite_debito_automatico = ~permite_debito_automatico
+		where nombre_medio_pago = @nombre_medio_de_pago
+	end
+end
+go
+
+-- Procedimiento para eliminar un medio de pago en la tabla
+create or alter procedure facturacion.eliminarMedioPago(@nombre_medio_de_pago varchar(40))
+as
+begin
+	if not exists(select 1 from facturacion.medio_de_pago
+				where nombre_medio_pago = @nombre_medio_de_pago)
+	begin
+		print 'El rol que se quiere eliminar, no existe segun su nombre.'
+	end
+	else
+	begin
+		delete from facturacion.medio_de_pago
+		where nombre_medio_pago = @nombre_medio_de_pago
+	end
+end
+go
+
+-- SOCIO
+-- Procedimiento para insertar un socio
+create or alter procedure socios.insertarSocio
+	@dni int,
+	@nombre varchar(40),
+	@apellido varchar(40),
+	@email varchar(150),
+	@fecha_nacimiento date,
+	@telefono_contacto int,
+	@telefono_emergencia int,
+	@id_obra_social int,
+	@id_categoria int,
+	@id_usuario int,
+	@id_medio_de_pago int
+as
+begin
+	if exists (select 1 from socios.socio 
+			   where dni = @dni)
+	begin
+		print 'ya existe un socio con ese dni.'
+	end
+	else
+	begin
+		insert into socios.socio
+		(dni, nombre, apellido, email, fecha_nacimiento, telefono_contacto, telefono_emergencia, habilitado,
+		 id_obra_social, id_categoria, id_usuario, id_medio_de_pago)
+		values
+		(@dni, @nombre, @apellido, @email, @fecha_nacimiento, @telefono_contacto, @telefono_emergencia, 1,
+		 @id_obra_social, @id_categoria, @id_usuario, @id_medio_de_pago)
+	end
+end
+go
+
+-- Procedimiento para modiifcar un socio
+create or alter procedure socios.modificarHabilitarSocio
+	@id_socio int
+as
+begin
+	if not exists (select 1 from socios.socio 
+					where id_socio = @id_socio)
+	begin
+		print 'No existe un socio con ese id.'
+	end
+	else
+	begin
+		/* Como tenemos un campo de 'bit' y solo tiene dos valores posibles, si lo quiero modificar, simplemente
+		   niego el bit actual vinculado con el habilitar */
+		update socios.socio
+		set habilitado = ~habilitado
+		where id_socio = @id_socio
+	end
+end
+go
+
+-- Procedimiento para eliminar un socio (mediante borrado logico)
+create or alter procedure socios.eliminarSocio
+	@DNI int
+as
+begin
+	if not exists (select 1 from socios.socio 
+					where DNI = @DNI)
+	begin
+		print 'No existe un socio con ese dni.'
+	end
+	else
+	begin
+		-- Borrado logico
+		update socios.socio 
+		set habilitado = 0
+	end
+end
+go
+
+-- USUARIO
+-- Procedimiento para insertar un usuario
+create or alter procedure socios.insertarUsuario
+	@id_rol int,
+	@contraseña varchar(40),
+	@fecha_vigencia_contraseña date
+as
+begin
+	insert into socios.usuario(id_rol, contraseña, fecha_vigencia_contraseña)
+	values (@id_rol, @contraseña, @fecha_vigencia_contraseña)
+end
+go
+
+-- Procedimiento para modificar la contraseña del usuario
+create or alter procedure socios.modificarContraseñaUsuario
+	@id_usuario int,
+	@contraseña varchar(40)
+as
+begin
+	if not exists (select 1 from socios.usuario 
+					where id_usuario = @id_usuario)
+	begin
+		print 'No existe un usuario con ese id.'
+	end
+	else
+	begin
+		update socios.usuario
+		set contraseña = @contraseña
+		where id_usuario = @id_usuario
+	end
+end
+go
+
+-- Procedimiento para modificar la fecha de vigencia de la contraseña
+create or alter procedure socios.modificarFechaVigenciaUsuario
+	@id_usuario int,
+	@fecha_vigencia_contraseña date
+as
+begin
+	if not exists (select 1 from socios.usuario 
+					where id_usuario = @id_usuario)
+	begin
+		print 'No existe un usuario con ese id.'
+	end
+	else
+	begin
+		if CONVERT(date, GETDATE()) < @fecha_vigencia_contraseña
+		update socios.usuario
+		set fecha_vigencia_contraseña = @fecha_vigencia_contraseña
+		where id_usuario = @id_usuario
+	end
+end
+go
+
+-- Procedimiento para eliminar usuarios
+create or alter procedure socios.eliminarUsuario
+	@id_usuario int
+as
+begin
+	if not exists (select 1 from socios.usuario where id_usuario = @id_usuario)
+	begin
+		print 'No existe un usuario con ese id.'
+	end
+	else
+	begin
+		delete from socios.usuario
+		where id_usuario = @id_usuario
+	end
+end
+go
+
+-- OBRA SOCIAL
+-- Procedimiento para insertar una obra social
+create or alter procedure socios.insertarObraSocial
+	@nombre_obra_social varchar(60),
+	@telefono_obra_social int
+as
+begin
+	if exists (select 1 from socios.obra_social 
+				where nombre_obra_social = @nombre_obra_social)
+	begin
+		print 'Ya existe una obra social con ese nombre.'
+	end
+	else
+	begin
+		insert into socios.obra_social(nombre_obra_social, telefono_obra_social)
+		values (@nombre_obra_social, @telefono_obra_social)
+	end
+end
+go
+
+-- Procedimiento para modificar una obra social
+create or alter procedure socios.modificarObraSocial
+	@nombre_obra_social varchar(60),
+	@telefono_obra_social int
+as
+begin
+	if not exists (select 1 from socios.obra_social 
+					where nombre_obra_social = @nombre_obra_social)
+	begin
+		print 'No existe una obra social con ese nombre.'
+	end
+	else
+	begin
+		update socios.obra_social
+		set telefono_obra_social = @telefono_obra_social
+		where nombre_obra_social = @nombre_obra_social
+	end
+end
+go
+
+-- Procedimiento para eliminar una obra social
+create or alter procedure socios.eliminarObraSocial
+	@nombre_obra_social varchar(60)
+as
+begin
+	if not exists (select 1 from socios.obra_social 
+					where nombre_obra_social = @nombre_obra_social)
+	begin
+		print 'No existe una obra social con ese nombre.'
+	end
+	else
+	begin
+		delete from socios.obra_social
+		where nombre_obra_social = @nombre_obra_social
+	end
+end
+go
+
+-- CATEGORIA
+-- Procedimiento para crear una categoria
+create or alter procedure socios.insertarCategoria
+	@nombre_categoria varchar(16),
+	@edad_minima int,
+	@edad_maxima int,
+	@costo_membresia decimal(9,3)
+as
+begin
+	if exists (select 1 from socios.categoria 
+				where nombre_categoria = @nombre_categoria)
+	begin
+		print 'Ya existe una categoría con ese nombre.'
+	end
+
+	else if @edad_minima >= @edad_maxima
+	begin
+		print 'Es incoherente que la edad minima sea mayor o igual que la maxima.'
+	end
+
+	else if @costo_membresia < 0
+	begin
+		print 'El costo de la membresia no puede ser negativo.'
+	end
+
+	else
+	begin
+		insert into socios.categoria(nombre_categoria, edad_minima, edad_maxima, costo_membresía)
+		values (@nombre_categoria, @edad_minima, @edad_maxima, @costo_membresia)
+	end
+end
+go
+
+-- Procedimiento para modificar el costo de una cateogira
+create or alter procedure socios.modificarCostoCategoria
+	@nombre_categoria varchar(16),
+	@costo_membresia decimal(9,3)
+as
+begin
+	if not exists (select 1 from socios.categoria where nombre_categoria = @nombre_categoria)
+	begin
+		print 'No existe una categoría con ese nombre.'
+	end
+	else
+	begin
+		update socios.categoria
+		set costo_membresía = @costo_membresia
+		where nombre_categoria = @nombre_categoria
+	end
+end
+go
+
+-- Procedimiento para eliminar una categoria
+create or alter procedure socios.eliminarCategoria
+	@nombre_categoria varchar(16)
+as
+begin
+	if not exists (select 1 from socios.categoria 
+					where nombre_categoria = @nombre_categoria)
+	begin
+		print 'No existe una categoría con ese nombre.'
+	end
+	else
+	begin
+		delete from socios.categoria
+		where nombre_categoria = @nombre_categoria
+	end
+end
+go
+
+-- RESPONSABLE MENOR
+-- Procedimiento para crear un responsable de un menor
+create or alter procedure socios.insertarResponsableMenor
+	@id_socio_menor int,
+	@nombre varchar(40),
+	@apellido varchar(40),
+	@dni int,
+	@email varchar(50),
+	@fecha_nacimiento date,
+	@telefono int,
+	@parentesco varchar(30)
+as
+begin
+	if not exists (select 1 from socios.socio 
+					where id_socio = @id_socio_menor)
+	begin
+		print 'No existe un socio con ese id.'
+	end
+
+	insert into socios.responsable_menor (
+		id_socio_menor, nombre, apellido,
+		dni, email, fecha_nacimiento, telefono, parentesco
+	)
+	values (
+		@id_socio_menor, @nombre, @apellido,
+		@dni, @email, @fecha_nacimiento, @telefono, @parentesco
+	)
+end
+go
+
+-- Procedimiento para eliminar un responsable de un menor
+create or alter procedure socios.eliminarResponsableMenor
+	@id_socio_responsable int
+as
+begin
+	if not exists (select 1 from socios.responsable_menor 
+					where id_socio_menor = @id_socio_responsable)
+	begin
+		print 'No existe un responsable de un menor con ese id.'
+	end
+
+	delete from socios.responsable_menor
+	where id_socio_responsable = @id_socio_responsable
+end
+go
